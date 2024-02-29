596c1d8384110c2b8f449223913f95396baa1606
FurinZya <161714383+FurinZya@users.noreply.github.com>
2024-03-01 00:41:52 +0300

    Add files via upload

---
+++ dvestroki.java
@@ -0,0 +1,15 @@
+public class dvestroki {
+    public static void main(String[] args) throws Exception {
+        String str1 = new String("I ate meat today");
+        String str2 = new String("I ate meat today");
+ 
+if (str1.equals(str2)) {
+    System.out.println("Строки идентичны");
+} else {
+    System.out.println("Строки неидентичны");
+}
+
+        
+    }
+
+}
