7d71c3647ea8661655363be9ce38d525074b65e7
FurinZya <161714383+FurinZya@users.noreply.github.com>
2024-02-29 15:40:52 +0300

    Add files via upload

---
+++ AiB.java
@@ -0,0 +1,16 @@
+public class AiB {
+    public static void main(String[] args) throws Exception {
+        int a = 1;
+        int b = 2;
+        if (a < b) {
+            System.out.println("a < b");
+        } else if (a > b) {
+            System.out.println("a > b");
+        } else {
+            System.out.println("a = b");
+        }
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+    }
+}
